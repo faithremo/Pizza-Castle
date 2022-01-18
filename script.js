47f7bff3177f087c3763903a8183ec35e960200c
faithremo <faithmakumi@gmail.com>
2022-01-19 01:16:49 +0300

    modifying and spellings

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//constructor for pizza orders
+//  pizza orders constructor
 class yourPizza {
     constructor( toppings, numberOfPizza,size, delivery,crust) {
         this.size = size;
@@ -10,44 +10,64 @@ class yourPizza {
 };
 //prototypes to get the prices of the pizza
 yourPizza.prototype.getSizePrice = function () {
-    if (this.size === "small") {
+    if (this.size === "small") 
+    {
         return 400;
-    } else if (this.size === "medium") {
+    } 
+    else if (this.size === "medium") {
         return 750;
-    } else if (this.size === "large") {
+    } 
+    else if (this.size === "large") {
         return 900;
-    } else if (this.size === "x-large") {
+    } 
+    else if (this.size === "x-large") {
         return 1100;
-    }else { return "please fill in the missing field"
+    }
+    else { 
+        return "please fill in the missing field!!!"
 }};
 yourPizza.prototype.getCrustPrice = function () {
     if (this.crust === "stuffed") {
         return 100;
-    } else if (this.crust === "cripsy") {
+    } 
+    else if (this.crust === "cripsy") {
         return 150;
-    } else if (this.crust === "gluten-free") {
+    } 
+    else if (this.crust === "gluten-free") {
         return 200;
-    } else{ return "please fill in the missing field"
+    } 
+    else{ 
+        return "please fill in the missing field!!!"
 }};
 yourPizza.prototype.getToppingsPrice = function () {
     if (this.toppings === "ChickenTikka") {
         return 200;
-    } else if (this.toppings === "BBQSteak") {
+    } 
+    else if (this.toppings === "BBQSteak") {
         return 150;
-    } else if (this.toppings === "ChickenandMushrooms") {
+    } 
+    else if (this.toppings === "ChickenandMushrooms") {
         return 250;
-    } else if (this.toppings === "BeefBBQ") {
+    } 
+    else if (this.toppings === "BeefBBQ") {
         return 100;
-    } else if (this.toppings === "Hawaiian") {
+    } 
+    else if (this.toppings === "Hawaiian") {
         return 100;
-    } else if (this.toppings === "ChickenPeri-Perin") {
+    } 
+    else if (this.toppings === "ChickenPeri-Perin") {
         return 220;
-    } else if (this.toppings === "VegTikka") {
+    } 
+    else if (this.toppings === "VegTikka") {
         return 70;
-    } else if (this.toppings === "Regina") {
+    } 
+    else if (this.toppings === "Regina") {
         return 180;
-    }else{return "please fill in the missing field"
-}};
+    }
+    else{
+        return "please fill in the missing field!!!"
+    }
+};
 yourPizza.prototype.priceOfDelivery = function () {
     if (this.delivery === "Hand") {
         return 0;
@@ -57,25 +77,27 @@ yourPizza.prototype.priceOfDelivery = function () {
 };
 // user feedback
 $(document).ready(function () {
-    $(".custom").submit(function (event) {
+    $(".specific").submit(function (event) {
         event.preventDefault();
-        var size = $("#inputSize option:selected").val();
-        var crust = $("#inputCrust option:selected").val();
-        var toppings = $("#inputToppings option:selected").val();
+        
+var size = $("#size option:selected").val();
+        var crust = $("#crust option:selected").val();
+        var toppings = $("#toppings option:selected").val();
         var numberOfPizza = parseInt($("#number").val());
-        var delivery = $("#inputDelivery option:selected").val();
+        var delivery = $("#delivery option:selected").val();
 
         var newyourPizza = new yourPizza(size, crust, toppings, numberOfPizza, delivery);
         var totalPrice = (newyourPizza.getSizePrice()+newyourPizza.getCrustPrice()+newyourPizza.getToppingsPrice())*numberOfPizza;
 
-        window.alert("Hi! You have ordered "+numberOfPizza+ " " +size+ " pizzas, with a " + crust +" crust and "+toppings + " toppings!");
-        window.alert("The total cost is  " + totalPrice + " /= " + " For Delivery services kindly fill the form below");
+        window.alert("Thank you! you ordered: "+numberOfPizza+ " " +size+ " pizzas, with a " + crust +" crust and "+toppings + " toppings!");
+        window.alert("The payable amount is  " + totalPrice + " /=. " + " Please fill the form below, for your order to be delivered right to your door-step.");
     
     $("#submit").click(function (event) {
         event.preventDefault();
-        var keyedName = $("#inputName").val();
-        var keyedAddress = $("#inputAddress").val();
-        window.alert("Hey" + " " + keyedName + " " + " Your order will be ready in 25 mins and will be delivered to" + " " + keyedAddress + " " + "in the next 45 mins.");
-        window.alert("Your total order will be " + (totalPrice + newyourPizza.priceOfDelivery()));
+        var keyedName = $("#name").val();
+        var keyedAddress = $("#address").val();
+        window.alert("Dear" + " " + keyedName + " " + " Your order will be delivered to" + " " + keyedAddress + " " + "in half an hour.");
+        window.alert("The total amount for your order is: " + (totalPrice + newyourPizza.priceOfDelivery()));
     });});
+    document.getElementsByClassName("specific").reset();
 });
